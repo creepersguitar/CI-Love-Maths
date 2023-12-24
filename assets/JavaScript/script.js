19ab97f2e9d0c8be7f8af44bfb20cb2de125a313
creepersguitar <>
2023-12-24 08:56:30 +0000

    added js structure

---
+++ assets/JavaScript/script.js
@@ -0,0 +1,29 @@
+function runGame() {
+
+}
+function checkAnswer() {
+
+}
+function calcCorrectAnswer() {
+
+}
+function incScore() {
+
+}
+function incWrongAnswer() {
+
+}
+function displayAddQuestion() {
+
+}
+function displayMinusQuestion() {
+
+}
+function displayTimesQuestion() {
+
+}
+
+// challenge to do by myself
+function displayDivideQuestion() {
+
+}
